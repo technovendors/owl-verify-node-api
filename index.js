858ccf7cb0d44bc9d6f923f5442dbb54b5790cbd
Nitin Goyal <nitingoyal.dev@gmail.com>
2018-10-07 18:08:23 +0530

    Initial list creation added

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const request = require('request');
-const querystring = require('querystring');
+let req = require('request'),
+    querystring = require('querystring');
 
-module.exports = (url, apiKey) => {
-  this.url = url;
-  this.apiKey = apiKey;
+module.exports = function(url, apiKey) {
+    this.url = url;
+    this.apiKey = apiKey;
 };
 
-let funObj = {
-    createList: (o, cb) => {
+let funcObj = {
+    createList: function(o, cb) {
         if (!o.list_name) cb(new Error('Missing "list_name" parameter'), null);
-
-        request({
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'x-api-key': this.apiKey,
-            },
-            url: this.url + 'email/lists',
-            body: querystring.stringify({
-                list_name: o.list_name
-            })
-        }, o, (err, res, body) => {
-            if (err) cb(err, null);
-            else {
-                cb(null, body);
-            }
-        });
+        else {
+            console.log(this.url + 'email/lists');
+            this.request('email/lists', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'x-api-key': this.apiKey,
+                },
+                body: querystring.stringify({
+                    list_name: o.list_name
+                })
+            }, function(err, res, body) {
+                if (err) cb(err, null);
+                else {
+                    cb(null, body);
+                }
+            });
+        }
     },
-    subscribe: (o, cb) => {
-
+    request: function(path, o,cb) {
+        req({
+            url: this.url + path,
+            method: o.method,
+            headers: o.headers,
+            body: o.body
+        }, cb);
     }
 };
 
-for (var k in funObj) module.exports.prototype[k] = funObj[k];
+for (var k in funcObj) module.exports.prototype[k] = funcObj[k];
